7f0f08acc0ec890c877f9cc4f653b7f70c744c40
LisaDesj <lisactoons@icloud.com>
2021-04-10 10:53:08 -0700

    Rehauled a lot
    Added sort by gender before ranking system, deleted unnecessary files

--- data_manipulation.pl
+++ data_manipulation.pl
@@ -3,29 +3,28 @@
 % seoncond recieve 3 points
 % third, 2 points
 % fourth, 1 point
-% argument: data, pref, newdata, age, loc, sex, kids
+% argument: data, pref, newdata, age, loc, kids
 
-score_by_pref([], _, [], _, _, _, _).
-score_by_pref([H|T], Pref, [V-H|T2], Age, Loc, Sex, Kids) :-
-  score_helper(H, Pref, V, 0, Age, Loc, Sex, Kids),
-  score_by_pref(T, Pref, T2, Age, Loc, Sex, Kids).
+score_by_pref([], _, [], _, _, _).
+score_by_pref([H|T], Pref, [V-H|T2], Age, Loc, Kids) :-
+  score_helper(H, Pref, V, 0, Age, Loc, Kids),
+  score_by_pref(T, Pref, T2, Age, Loc, Kids).
 
 % score_helper()
-score_helper(_, _, 0, 4, _, _, _, _).
-score_helper(H, Pref, V, Ind, Age, Loc, Sex, Kids) :-
+score_helper(_, _, 0, 4, _, _, _).
+score_helper(H, Pref, V, Ind, Age, Loc, Kids) :-
   NInd is Ind + 1,
-  (check_attr(H, Pref, Ind, Age, Loc, Sex, Kids) ->
-    score_helper(H, Pref, K, NInd, Age, Loc, Sex, Kids),
+  (check_attr(H, Pref, Ind, Age, Loc, Kids) ->
+    score_helper(H, Pref, K, NInd, Age, Loc, Kids),
     V is (1 * (10 - Ind)) + K
     ;
-    score_helper(H, Pref, V, NInd, Age, Loc, Sex, Kids)).
+    score_helper(H, Pref, V, NInd, Age, Loc, Kids)).
 
 
 
-check_attr(H, Pref, Ind, (FS, SS), Loc, Sex, Kids) :-
+check_attr(H, Pref, Ind, (FS, SS), Loc, Kids) :-
   nth0(Ind, Pref, Attr),
   (Attr == "Loc" -> loc(H, Loc);
-  Attr == "Sex" -> sex(H, Sex);
   Attr == "Kids" -> kids(H, Kids);
     age(H, A2),
     A2 >= FS,
@@ -36,11 +35,9 @@ check_attr(H, Pref, Ind, (FS, SS), Loc, Sex, Kids) :-
 % for each criterial that the user satisfies, add 1
 % NOTE: "maybe" in having kids is treated as neither (no points awards either way)
 % NOTE: "B" in want gender is reated as both (points awarded either way)
-score_again([], [],  _, _, _, _, _).
-score_again([V-H| T1], [Y-H| T2], Age, Kids, Sex, Wk, Ws) :-
+score_again([], [],  _, _, _).
+score_again([V-H| T1], [Y-H| T2], Age, Kids, Wk) :-
   wAge(H, HW),
-  wSex(H, HS),
-  sex(H, MS),
   get_from_wage(HW, FS, SS),
   (Age >= FS,
    SS >= Age ->
@@ -49,18 +46,12 @@ score_again([V-H| T1], [Y-H| T2], Age, Kids, Sex, Wk, Ws) :-
   (wKids(H, HK), HK == Kids ->
     (kids(H, Wk) -> V2 is 5; V2 is 2);
     V2 is -1),
-  (atom_string(HS, "B") ->
-    (atom_string(MS, MSS), MSS == Ws ->  V3 is 30; V3 is 2)
-    ;
-    wSex(H, Sex) ->
-      (atom_string(MS, MSS), MSS == Ws -> V3 is 30; V3 is 2);
-      V3 is -3),
   % blob(HS, TT),
   % % (string(HS) -> write("HS is string \n"); write("Not string \n")),
   % wSex(H, Q),
   % write(wSex(H, Sex)), nl,
-  Y is V + V1 + V2 + V3,
-  score_again(T1, T2, Age, Kids, Sex, Wk, Ws).
+  Y is V + V1 + V2,
+  score_again(T1, T2, Age, Kids, Wk).
 
 % to get the lowerbond and upper bond from WAge
 get_from_wage(WAge, FS, SS) :-
---
+++ date.pl
@@ -0,0 +1,139 @@
+:- [take_input].
+:- [data_manipulation].
+%:- [sortBySex].
+
+% start takes in User Input to create a Blind Date profile and possibly look for matches
+% type period and enter after the answer (when the user writes)
+% user needs to write words encased in ' '.
+
+start :- 
+    write("Welcome to Blind Date."), nl,
+    take_name(N),
+    take_age(A),
+    take_sex(Sex),
+    take_Ori(Ori),
+    take_BT(BT),
+    take_Edu(Edu),
+    take_Eth(Eth),
+    take_height(H),
+    take_loc(L),
+    take_kids(K),
+    take_WAge(WAge),
+    take_WSex(WSex),
+    take_WKids(WKids),
+    take_ready(Answer),
+    (Answer == "Y" -> 
+        getMatches(N,A,Sex,Ori,BT,Edu,Eth,H,L,K,WAge,WSex,WKids),
+        createProfile(N,A,Sex,Ori,BT,Edu,Eth,H,L,K,WAge,WSex,WKids)
+        ; write("Thanks for creating your profile. Restart Blind Date to look for matches!")).
+
+% write profile to csv
+createProfile(N,A,Sex,Ori,BT,Edu,Eth,H,L,K,WAge,WSex,WKids) :-
+    csv_read_file('trialDB02.csv', Data),
+    append(Data, [row(N,A,Sex,Ori,BT,Edu,Eth,H,L,K,WAge,WSex,WKids)],X),
+    csv_write_file('trialDB02.csv',X).
+
+% getMatches takes in a profile and outputs a list of matches
+getMatches(N,A,Sex,Ori,BT,Edu,Eth,H,L,K,WAge,WSex,WKids) :-
+    csv_read_file("trialDB02.csv", [_|Data]),
+    wSex3(Data,Sex,WSex,V),
+    %write(Sex),
+    %write(V),
+    take_pref(Pref),
+    get_from_wage(WAge, FS, SS),
+    % SP_Data : scored by preference data
+    score_by_pref(V, Pref, SP_Data, (FS, SS), Loc, WKids),
+    % SA_Data : data score again
+    score_again(SP_Data, SA_Data, A, K, WKids),
+    sort(0, @>=, SA_Data, Sorted_Data),
+    show_result(X, Sorted_Data).
+    %------------------------
+    % % Used for rating system debugging
+     %print_data(Sorted_Data).
+    %------------------------
+
+% to get index of match to view
+show_result(X, Sorted_Data) :-
+  write("
+        To view one of your top 10 matches, enter a integer in the range [1,10].
+        To exit, enter \"quit\"."),
+  nl,
+  read(Y),
+  (Y == "quit" ->
+    X = Y;
+    (integer(Y),
+    Y >= 1,
+    10 >= Y ->
+        Ind is Y - 1,
+        nth0(Ind, Sorted_Data, Profile),
+        first(Profile, Score),
+        second(Profile, Info),
+        % name(Info, Name),
+        % age(Info Age),
+        % sex(Info, Sex),
+        % bt(Info, BT),
+        % edu(Info, Edu),
+        % eth(Info, Eth),
+        % height(Info, H),
+        % loc(Info, Loc),
+        % kids(Info, Kids),
+        % write(Profile),
+        write("\n \n ------------------------------------- \n"),
+        write("Score "), write(Score),
+        nl,
+        write("Profile "), write(Info),
+        write("\n ------------------------------------- \n \n");
+    write("Invalid input, try again. \n")),
+    show_result(X, Sorted_Data)).
+
+%wSex3 parses 'trialDB02.csv' for a list of matches based on Sex and wSex.
+
+wSex3([row(X,A,'m',O,BT,ED,ET,H,L,K,WA,"b",WK)],_,"b",[row(X,A,'m',O,BT,ED,ET,H,L,K,WA,'b',WK)]).
+wSex3([row(X,A,'f',O,BT,ED,ET,H,L,K,WA,'b',WK)],_,"b",[row(X,A,'f',O,BT,ED,ET,H,L,K,WA,'b',WK)]).
+wSex3([row(X,A,'m',O,BT,ED,ET,H,L,K,WA,Sex,WK)],Sex,"b",[row(X,A,'m',O,BT,ED,ET,H,L,K,WA,Sex,WK)]).
+wSex3([row(X,A,'f',O,BT,ED,ET,H,L,K,WA,Sex,WK)],Sex,"b",[row(X,A,'f',O,BT,ED,ET,H,L,K,WA,Sex,WK)]).
+wSex3([row(X,A,AS,O,BT,ED,ET,H,L,K,WA,BS,WK)],Sex,WSex,[]) :- dif(AS,WSex),dif(BS,Sex),dif(Sex,WSex).                             %dif(WSex,b).
+wSex3([row(X,_,'m',_,_,_,_,_,_,_,_,_,_)],"f","f",[]). 
+wSex3([row(X,_,'f',_,_,_,_,_,_,_,_,_,_)],"m","m",[]).
+wSex3([row(X,_,'f',_,_,_,_,_,_,_,_,'m',_)],"f","f",[]).  
+wSex3([row(X,_,'m',_,_,_,_,_,_,_,_,'f',_)],"m","m",[]). 
+wSex3([row(X,A,'f',O,BT,ED,ET,H,L,K,WA,'m',WK)],"m","f",[row(X,A,"m",O,BT,ED,ET,H,L,K,WA,"f",WK)]).
+wSex3([row(X,A,'m',O,BT,ED,ET,H,L,K,WA,'f',WK)],"f","m",[row(X,A,"m",O,BT,ED,ET,H,L,K,WA,"f",WK)]).
+wSex3([row(X,A,'f',O,BT,ED,ET,H,L,K,WA,'f',WK)],"f","f",[row(X,A,"f",O,BT,ED,ET,H,L,K,WA,"f",WK)]).
+wSex3([row(X,A,'f',O,BT,ED,ET,H,L,K,WA,'f',WK)],"f","f",[row(X,A,"f",O,BT,ED,ET,H,L,K,WA,"b",WK)]).
+wSex3([row(X,A,'m',O,BT,ED,ET,H,L,K,WA,'m',WK)],"m","m",[row(X,A,"m",O,BT,ED,ET,H,L,K,WA,"m",WK)]).
+wSex3([row(X,A,'m',O,BT,ED,ET,H,L,K,WA,'b',WK)],"m","m",[row(X,A,"m",O,BT,ED,ET,H,L,K,WA,"m",WK)]).
+wSex3([row(X,A,WSex,O,BT,ED,ET,H,L,K,WA,Sex,WK)],Sex,WSex,[row(X,A,WSex,O,BT,ED,ET,H,L,K,WA,Sex,WK)]).
+
+
+wSex3([row(X,A,'m',O,BT,ED,ET,H,L,K,WA,'b',WK)|T],Sex,"b",[row(X,A,'m',O,BT,ED,ET,H,L,K,WA,'b',WK)|V]) :- 
+    wSex3(T,Sex,"b",V),!.
+wSex3([row(X,A,'f',O,BT,ED,ET,H,L,K,WA,'b',WK)|T],Sex,"b",[row(X,A,'f',O,BT,ED,ET,H,L,K,WA,'b',WK)|V]) :- 
+    wSex3(T,Sex,"b",V),!.
+wSex3([row(X,A,'f',O,BT,ED,ET,H,L,K,WA,"f",WK)|T],"f","b",[row(X,A,'f',O,BT,ED,ET,H,L,K,WA,'f',WK)|V]) :- 
+    wSex3(T,"f","b",V),!.
+wSex3([row(X,A,'m',O,BT,ED,ET,H,L,K,WA,'f',WK)|T],"f","b",[row(X,A,'m',O,BT,ED,ET,H,L,K,WA,'f',WK)|V]) :- 
+    wSex3(T,"f","b",V),!.
+wSex3([row(X,A,'m',O,BT,ED,ET,H,L,K,WA,'m',WK)|T],"m","b",[row(X,A,'m',O,BT,ED,ET,H,L,K,WA,'m',WK)|V]) :- 
+    wSex3(T,"m","b",V),!.
+wSex3([row(X,A,'f',O,BT,ED,ET,H,L,K,WA,m,WK)|T],"m","b",[row(X,A,'f',O,BT,ED,ET,H,L,K,WA,'m',WK)|V]) :- 
+    wSex3(T,"m","b",V),!.
+wSex3([row(X,A,'f',O,BT,ED,ET,H,L,K,WA,'m',WK)|T],"m","f",[row(X,A,'f',O,BT,ED,ET,H,L,K,WA,'m',WK)|V]) :- 
+    wSex3(T,"m","f",V),!.
+wSex3([row(X,A,'m',O,BT,ED,ET,H,L,K,WA,'f',WK)|T],"f","m",[row(X,A,'m',O,BT,ED,ET,H,L,K,WA,'f',WK)|V]) :- 
+    wSex3(T,"f","m",V),!.
+wSex3([row(X,A,'m',O,BT,ED,ET,H,L,K,WA,'m',WK)|T],"m","m",[row(X,A,'m',O,BT,ED,ET,H,L,K,WA,'m',WK)|V]) :- 
+    wSex3(T,"m","m",V),!.
+wSex3([row(X,A,'m',O,BT,ED,ET,H,L,K,WA,'b',WK)|T],"m","m",[row(X,A,'m',O,BT,ED,ET,H,L,K,WA,'b',WK)|V]) :- 
+    wSex3(T,"m","m",V),!.
+wSex3([row(X,A,'f',O,BT,ED,ET,H,L,K,WA,'f',WK)|T],"f","f",[row(X,A,'f',O,BT,ED,ET,H,L,K,WA,'f',WK)|V]) :- 
+    wSex3(T,"f","f",V),!.
+wSex3([row(X,A,'f',O,BT,ED,ET,H,L,K,WA,'b',WK)|T],"f","f",[row(X,A,'f',O,BT,ED,ET,H,L,K,WA,'b',WK)|V]) :- 
+    wSex3(T,"f","f",V),!.
+wSex3([row(X,A,WSex,O,BT,ED,ET,H,L,K,WA,Sex,WK)|T],Sex,WSex,[row(X,A,WSex,O,BT,ED,ET,H,L,K,WA,Sex,WK)|V]) :- 
+    wSex3(T,Sex,WSex,V),!.
+wSex3([row(X,A,Sex,O,BT,ED,ET,H,L,K,WA,Sex,WK)|T],Sex,Sex,[row(X,A,Sex,O,BT,ED,ET,H,L,K,WA,Sex,WK)|V]) :- 
+    wSex3(T,Sex,Sex,V),!.
+wSex3([_|T],Sex,'b',X) :- wSex3(T,Sex,"b",X).
+wSex3([_|T],Sex,WSex,X) :- wSex3(T,Sex,WSex,X).
+
